17c9069c6149308ff712072c4d9c1bb5a94e02ae
RiddhiChipade <riddhichipade@gmail.com>
2026-07-08 20:47:50 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/riddhic/aiengineering/AiEngineeringWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.riddhic.aiengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngineeringWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEngineeringWorkspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riddhic/aiengineering/AiEngineeringWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riddhic.aiengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEngineeringWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
